926907398287b75b1cc6b4e5c644910e23b57d7c
Matheus Oliveira da Silva <matheus.6148@gmail.com>
2025-11-25 13:15:57 -0300

    feat: fdw setup to simplify data aggregation

---
+++ webshop_analytics/fdw_setup.sql
@@ -0,0 +1,32 @@
+-- FDW SETUP
+CREATE EXTENSION IF NOT EXISTS postgres_fdw;
+
+CREATE SERVER webshop_server
+    FOREIGN DATA WRAPPER postgres_fdw
+    OPTIONS (
+        host 'localhost',
+        port '5432',
+        dbname 'web_shop'
+    );
+
+CREATE USER MAPPING FOR postgres
+    SERVER webshop_server
+    OPTIONS (
+        user 'postgres',
+        password ''
+    );
+
+CREATE FOREIGN TABLE orders_foreign (
+    order_id INTEGER NOT NULL,
+    customer_id INTEGER NOT NULL,
+    order_date TIMESTAMP WITHOUT TIME ZONE,
+    order_status VARCHAR(50) NOT NULL,
+    shipping_cost NUMERIC(10, 2)
+)
+SERVER webshop_server
+OPTIONS (
+    schema_name 'public',
+    table_name 'orders'
+);
+
+select * from orders_foreign;
