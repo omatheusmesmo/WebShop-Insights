8055c07521d903d7d81c322aaff9ff6a31aad731
Matheus Oliveira da Silva <matheus.6148@gmail.com>
2025-11-27 17:30:34 -0300

    chore: initial setup

---
+++ pipelines/java/product-ranking-batch/src/main/java/br/com/analytics/pipeline/product_ranking_batch/ProductRankingBatchApplication.java
@@ -0,0 +1,13 @@
+package br.com.analytics.pipeline.product_ranking_batch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductRankingBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductRankingBatchApplication.class, args);
+	}
+
+}
