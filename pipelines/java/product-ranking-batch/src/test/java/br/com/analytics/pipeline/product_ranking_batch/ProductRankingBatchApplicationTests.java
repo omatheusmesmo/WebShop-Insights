abe0049ec1fcb64e2870120c12e52e6435afc9a3
Matheus Oliveira da Silva <matheus.6148@gmail.com>
2025-11-27 17:32:05 -0300

    test: add unit tests for ProductRankingBatch application context

---
+++ pipelines/java/product-ranking-batch/src/test/java/br/com/analytics/pipeline/product_ranking_batch/ProductRankingBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.analytics.pipeline.product_ranking_batch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductRankingBatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
