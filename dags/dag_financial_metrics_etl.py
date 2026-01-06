caee16b8d8f421ddcac8617e9149906809b2fbfe
Matheus Oliveira <matheus.6148@gmail.com>
2026-01-06 18:44:48 -0300

    fix: update start_date and schedule in financial_metrics_etl DAG

--- dags/dag_financial_metrics_etl.py
+++ dags/dag_financial_metrics_etl.py
@@ -2,13 +2,12 @@ from __future__ import annotations
 import pendulum
 from airflow.models.dag import DAG
 from airflow.operators.python import PythonOperator
-
 from pipelines.python.financial_metrics import run_pipeline
 
 with DAG(
         dag_id="financial_metrics_etl",
-        start_date=pendulum.datetime(2025, 1, 1, tz="UTC"),
-        schedule="@daily",
+        start_date=pendulum.datetime(2026, 1, 1, tz="UTC"),
+        schedule=None,
         catchup=False,
         tags=["python", "etl", "financial"],
 ) as dag:
