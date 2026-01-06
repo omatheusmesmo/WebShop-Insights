7f66a7a17bbdb82e5c8d3081fd26abf85d119156
Matheus Oliveira <matheus.6148@gmail.com>
2026-01-06 18:44:53 -0300

    fix: update start_date in order_subtotals_etl DAG and add task dependencies

--- dags/dag_order_subtotals_etl.py
+++ dags/dag_order_subtotals_etl.py
@@ -2,21 +2,33 @@ from __future__ import annotations
 import pendulum
 from airflow.models.dag import DAG
 from airflow.operators.python import PythonOperator
-
-# 1. IMPORTAÇÃO DA LÓGICA
-# O Airflow adiciona a pasta 'plugins' ao PYTHONPATH, permitindo a importação.
+from airflow.sensors.external_task import ExternalTaskSensor
+from airflow.operators.trigger_dagrun import TriggerDagRunOperator
 from pipelines.python.order_subtotals import run_pipeline
 
 with DAG(
         dag_id="order_subtotals_etl",
-        start_date=pendulum.datetime(2025, 1, 1, tz="UTC"),
+        start_date=pendulum.datetime(2026, 1, 1, tz="UTC"),
         schedule="@daily",
         catchup=False,
         tags=["python", "etl", "orders"],
 ) as dag:
-    # 2. CHAMADA DA FUNÇÃO PRINCIPAL
+
+    wait_for_init = ExternalTaskSensor(
+        task_id="wait_for_data_population",
+        external_dag_id="data_population_etl",
+        external_task_id="populate_webshop_db",
+        execution_date_fn=lambda dt: pendulum.datetime(2026, 1, 1, tz="UTC"),
+    )
+
     process_subtotals = PythonOperator(
         task_id="calculate_subtotals_task",
-        # Chama a função run_pipeline que contém toda a sua lógica E/T/L
         python_callable=run_pipeline,
     )
+
+    trigger_financial = TriggerDagRunOperator(
+        task_id="trigger_financial_metrics",
+        trigger_dag_id="financial_metrics_etl"
+    )
+
+    wait_for_init >> process_subtotals >> trigger_financial
